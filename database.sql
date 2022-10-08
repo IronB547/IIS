6d125fb12b5c3d600acfd9868d6d6da6f01e9205
Juraj Dedič <jurajdedic@gmail.com>
2022-10-08 19:20:24 +0200

    fixed

--- database.sql
+++ database.sql
@@ -2,30 +2,46 @@ CREATE TABLE Users (
     id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    user_type INT NOT NULL,
+    user_type ENUM ('admin', 'user', 'city manager', 'technician') NOT NULL,
     PRIMARY KEY (id)
 );
+
+#trigger to hash password
+CREATE TRIGGER hash_password
+BEFORE INSERT ON Users
+FOR EACH ROW
+BEGIN
+    SET NEW.password = SHA2(NEW.password, 256);
+END;
 
 CREATE TABLE Ticket (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(1023) NOT NULL,
-    status VARCHAR(255) NOT NULL,
+    status ENUM ('open', 'waiting', 'solved', 'rejected') NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Service_request (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(1023) NOT NULL,
-    solution_time VARCHAR(255) NOT NULL,
-    solution_state VARCHAR(255) NOT NULL,
-    time_spent VARCHAR(255) NOT NULL,
-    user_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    solution_time VARCHAR(255) DEFAULT '0',
+    solution_state ENUM ('unsolved', 'solved') NOT NULL DEFAULT 'unsolved',
+    time_spent VARCHAR(255) DEFAULT '0',
+    technician_id INT NOT NULL,
+    city_manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Service_request_technician (
+    service_request_id INT NOT NULL,
+    technician_id INT NOT NULL,
+    PRIMARY KEY (service_request_id, technician_id),
+    FOREIGN KEY (service_request_id) REFERENCES Service_request(id) ON DELETE CASCADE,
+    FOREIGN KEY (technician_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Ticket_photo (
@@ -33,7 +49,7 @@ CREATE TABLE Ticket_photo (
     url VARCHAR(511) NOT NULL,
     ticket_id INT NOT NULL,
     PRIMARY KEY (id, ticket_id),
-    FOREIGN KEY (ticket_id) REFERENCES Ticket(id)
+    FOREIGN KEY (ticket_id) REFERENCES Ticket(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Ticket_comment (
@@ -43,8 +59,8 @@ CREATE TABLE Ticket_comment (
     ticket_id INT NOT NULL,
     user_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (ticket_id) REFERENCES Ticket(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    FOREIGN KEY (ticket_id) REFERENCES Ticket(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Service_request_comment (
@@ -58,3 +74,29 @@ CREATE TABLE Service_request_comment (
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
 
+
+#function to drop all tables
+CREATE PROCEDURE delete_all_tables()
+BEGIN
+    DROP TABLE IF EXISTS Ticket_comment;
+    DROP TABLE IF EXISTS Ticket_photo;
+    DROP TABLE IF EXISTS Service_request_comment;
+    DROP TABLE IF EXISTS Service_request_technician;
+    DROP TABLE IF EXISTS Service_request;
+    DROP TABLE IF EXISTS Ticket;
+    DROP TABLE IF EXISTS Users;
+END
+
+#run the function
+CALL delete_all_tables();
+
+DELETE FROM Users where id = 1;
+
+
+#insert sample data
+
+INSERT INTO Users (username, password, user_type) VALUES ('admin', 'admin', 'admin');
+
+INSERT INTO Ticket (title, description, status, user_id) VALUES ('Ticket 1', 'Description 1', 'Open', 1);
+
+INSERT INTO Ticket_comment (comment, created_at, ticket_id, user_id) VALUES ('Fixed the street light', '2019-01-01 00:00:00', 1, 1);
